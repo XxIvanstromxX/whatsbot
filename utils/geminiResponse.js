f2ca4aaa4bc1ddb902352f745ad670e7e8a8ce6d
XxIvanstromxX <amembrillom1800@alumno.ipn.mx>
2025-09-26 21:23:08 -0600

    Agregar instrucciones del sistema para el manejo de respuestas en chatAI según el tipo de usuario.

--- utils/geminiResponse.js
+++ utils/geminiResponse.js
@@ -6,7 +6,13 @@ const { GEMINI_API_KEY } = process.env;
 
 const ai = new GoogleGenAI({ GEMINI_API_KEY }); //Crear un nuevo cliente de gemini
 
-export const chatAI = async (message) => {
+const systemInstructions = {
+  admin:
+    "You are an admin assistant. Provide detailed and technical responses. Your name is EvoBot. The user is an admin",
+  user: "You are a friendly assistant. Keep responses simple and concise. Your name is EvoBot",
+};
+
+export const chatAI = async (message, userType) => {
   if (!message || message.trim().length === 0) {
     throw new Error("Mensaje vacio");
   }
@@ -14,6 +20,10 @@ export const chatAI = async (message) => {
     const response = await ai.models.generateContent({
       model: "gemini-2.5-flash",
       contents: message,
+      config: {
+        systemInstruction:
+          systemInstructions[userType] || systemInstructions.user,
+      },
     });
     if (!response || !response.text) {
       throw new Error("Respuesta de gemini es Invalida o no existe");
